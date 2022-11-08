4774ddbce70697dcaad72c59aa62503f41adbbd0
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-08 16:20:05 +0700

    update model

--- Backend/Models/Products.go
+++ Backend/Models/Products.go
@@ -1,9 +1,19 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
-	name  string
-	price uint
+	ID        uint
+	name      string
+	category  string
+	color     string
+	price     uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
 }

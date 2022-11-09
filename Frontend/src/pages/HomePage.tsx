6b950a97ff6cbd5c29b681640985bfcfa2a6d238
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-09 10:39:11 +0700

    set header

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavbarMenu from "../components/navbar/NavbarMenu";
+import Header from "../components/home/Header";
 
 export default function () {
   return (
     <>
       <NavbarMenu />
-      {"home"}
+      <Header />
     </>
   );
 }

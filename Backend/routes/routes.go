3f4fadf1d40e433bc73e450619276668f037fca2
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-11-28 17:29:17 +0700

    setup cors

--- Backend/routes/routes.go
+++ Backend/routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	ProductController "Backend/api/controllers/product"
+	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -9,7 +11,24 @@ import (
 
 func Router() {
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("ENDPOINT_URL")},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == os.Getenv("ENDPOINT_URL")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	//auth
+	auth := r.Group("/api/auth")
+	{
+		auth.GET("/login", ProductController.ReadProduct)
+		auth.POST("/register", ProductController.Create)
+	}
 
 	// product
 	product := r.Group("/api/product")

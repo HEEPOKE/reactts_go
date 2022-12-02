a8ee1fc8a9cdf45b950d122559b1c0e7f9c42eb1
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-03 05:28:20 +0700

    edit axios

--- Frontend/src/https/auth.ts
+++ Frontend/src/https/auth.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const httpAuth = axios.create({
-  baseURL: "127.0.0.1:6476/api/auth",
+  baseURL: "http://localhost/:6476/api/auth",
   headers: {
     "Content-type": "application/json",
-    accept: "application/json",
-    // accept: "*/*",
+    // accept: "application/json",
   },
 });

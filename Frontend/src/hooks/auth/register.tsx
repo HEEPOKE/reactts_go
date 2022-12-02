bb67f3377412cfeab1d51e87c72b5aa4f006af0c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-02 10:26:46 +0700

    set axios register

--- Frontend/src/hooks/auth/register.tsx
+++ Frontend/src/hooks/auth/register.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import UserInterface from "../../interfaces/UserInterface";
+import { errorAuth } from "../../utils/auth/errorAuth";
 
-export default function register() {
-  return (
-    <div>register</div>
-  )
-}
+export const Register = async (
+  username: string,
+  email: string,
+  password: string,
+  tel: string
+): Promise<UserInterface> => {
+  const data: UserInterface = await axios
+    .post("http://localhost:6476/api/auth/register", {
+      username,
+      email,
+      password,
+      tel,
+    })
+    .then((res: any) => res.data)
+    .catch((err: any) => errorAuth(err));
+
+  return data;
+};
--- Frontend/src/utils/auth/errorAuth.tsx
+++ Frontend/src/utils/auth/errorAuth.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-const errorAuth = (error: any) => {
+export const errorAuth = (error: any) => {
   if (error?.response?.data?.error) {
     throw error.response.data.error;
   }
   throw error;
 };
 
-export default errorAuth;

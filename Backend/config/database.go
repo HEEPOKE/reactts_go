245df7b6c63b1cf9f8c4ac334382b0fffbc3cfbb
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-04 03:24:30 +0700

    setup new host

--- Backend/config/database.go
+++ Backend/config/database.go
@@ -12,9 +12,7 @@ var DB *gorm.DB
 var err error
 
 func Database() {
-	// dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("LOCALHOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"
-	dsn := os.Getenv("DB_USER") + ":@tcp(" + os.Getenv("LOCALHOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"
-	// dsn := "root:yoyo5555@tcp(localhost)/goapi?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("LOCALHOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {

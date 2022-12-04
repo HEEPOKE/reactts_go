a510d6501157aaa4a850b6f3ee770409907c695b
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-05 03:13:53 +0700

    update ui add product

--- Frontend/src/components/createProduct/FormInput.tsx
+++ Frontend/src/components/createProduct/FormInput.tsx
@@ -1,69 +1,71 @@
 import React from "react";
-import { Container, Row, Form, Button } from "react-bootstrap";
+import { Container, Row, Form, Button, Card } from "react-bootstrap";
 import "../../assets/css/product/create.css";
 
 export default function FormInput() {
   return (
     <Container>
-      <Row>
-        <div className="col-md-3 bg-warning">
-          <div className="contact-info">
-            <img
-              src="https://image.ibb.co/kUASdV/contact-image.png"
-              alt="image"
-            />
-            <h2>Contact Us</h2>
-            <h4>We would love to hear from you !</h4>
-          </div>
-        </div>
-        <div className="col-md-9">
-          <div className="contact-form">
-            <Form.Group className="form-group">
-              <Form.Label className="control-label col-sm-2">
-                First Name:
-              </Form.Label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter First Name"
-                />
+      <Card className="mt-5">
+        <Card.Body>
+          <Row>
+            <div className="col-md-3">
+              <div className="contact-info">
+                <h2>Add</h2>
+                <h2>Products</h2>
               </div>
-            </Form.Group>
-            <Form.Group className="form-group">
-              <Form.Label className="control-label col-sm-2">
-                Last Name:
-              </Form.Label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Last Name"
-                />
+            </div>
+            <div className="col-md-9">
+              <div className="contact-form">
+                <Form.Group className="form-group">
+                  <Form.Label className="control-label col-sm-2">
+                    First Name:
+                  </Form.Label>
+                  <div className="col-sm-10">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter First Name"
+                    />
+                  </div>
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Form.Label className="control-label col-sm-2">
+                    Last Name:
+                  </Form.Label>
+                  <div className="col-sm-10">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Last Name"
+                    />
+                  </div>
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Form.Label className="control-label col-sm-2">
+                    Email:
+                  </Form.Label>
+                  <div className="col-sm-10">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Enter email"
+                      name="email"
+                    />
+                  </div>
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <div className="col-sm-offset-2 col-sm-10">
+                    <Button type="submit" className="btn btn-default">
+                      Submit
+                    </Button>
+                  </div>
+                </Form.Group>
               </div>
-            </Form.Group>
-            <Form.Group className="form-group">
-              <Form.Label className="control-label col-sm-2">Email:</Form.Label>
-              <div className="col-sm-10">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder="Enter email"
-                  name="email"
-                />
-              </div>
-            </Form.Group>
-            <Form.Group className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
-                <Button type="submit" className="btn btn-default">
-                  Submit
-                </Button>
-              </div>
-            </Form.Group>
-          </div>
-        </div>
-      </Row>
+            </div>
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
--- Frontend/src/components/createProduct/Header.tsx
+++ Frontend/src/components/createProduct/Header.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function HeaderCreate() {
   return (
-    <div className="text-center mt-3">
-    <h2>Create Shirt</h2>
-  </div>
-  )
+    <div className="text-center mt-4">
+      <h2 className="text-white">Create Shirt</h2>
+    </div>
+  );
 }

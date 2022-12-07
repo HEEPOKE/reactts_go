59b59d3f05caf694b65a52480e2cc0675c70424a
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-07 09:48:11 +0700

    Update product.ts

--- Frontend/src/utils/product.ts
+++ Frontend/src/utils/product.ts
@@ -1,10 +1,19 @@
 import Swal from "sweetalert2";
 
+function historyBack() {
+  window.history.back();
+}
+
 const readErr = (err: any) => {
   Swal.fire({
     icon: "error",
     title: "Server Error",
     text: err,
+    showConfirmButton: true,
+    confirmButtonText: "Back",
+  }).then((res: any) => {
+    if (res.isConfirmed) {
+      historyBack();
   });
 };
 

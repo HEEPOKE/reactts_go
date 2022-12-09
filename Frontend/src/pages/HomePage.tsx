f0f87ec223c3c34d9e3ce5aa2a7705fd15e2e488
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-10 06:53:05 +0700

    Update HomePage.tsx

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -2,9 +2,6 @@ import NavbarMenu from "../components/navbar/NavbarMenu";
 import Header from "../components/home/Header";
 
 export default function HomePage() {
-  const access_token = sessionStorage.getItem("access_token") ?? null;
-  console.log(`Bearer ${access_token}`);
-
   return (
     <>
       <NavbarMenu />

ec177f123745179d469fd546e61e3cef35851e85
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-10 06:29:22 +0700

    Update http.ts

--- Frontend/src/https/http.ts
+++ Frontend/src/https/http.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+const access_token = sessionStorage.getItem("access_token") ?? null;
+
 const http = axios.create({
   // baseURL: process.env.BASE_BACKEND_URL,
   baseURL: "http://localhost:6476",
   headers: {
     "Content-type": "application/json",
+    Authorization: "Bearer " + access_token,
   },
 });
 

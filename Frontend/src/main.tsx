e4e5b199902ca90c3068682f4c6047311635aa67
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-11 00:08:38 +0700

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/routes";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas, faSquarePen, faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import {
+  fas,
+  faSquarePen,
+  faTrashCan,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/loading.css";
 import "./assets/css/Auth.css";
 
-library.add(fas, faSquarePen, faTrashCan);
+library.add(fas, faSquarePen, faTrashCan, faUser);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <AppRouter />

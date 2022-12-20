6260b433b11e22af977ef4f2a108c81df9333426
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-20 09:11:42 +0700

    check token

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,11 +1,16 @@
 import NavbarMenu from "../components/navbar/NavbarMenu";
 import Header from "../components/home/Header";
+import { Button } from "react-bootstrap";
+import authHeader from "../services/auth/AuthHeader";
 
 export default function HomePage() {
   return (
     <>
       <NavbarMenu />
       <Header />
+      <Button className="btn col-6" onClick={authHeader}>
+        go
+      </Button>
     </>
   );
 }
--- Frontend/src/services/auth/AuthHeader.ts
+++ Frontend/src/services/auth/AuthHeader.ts
@@ -2,8 +2,8 @@ export default function authHeader() {
   const access_token = sessionStorage.getItem("access_token") ?? false;
 
   if (!access_token) {
-    return { Authorization: "Bearer " + access_token };
+    return "";
   } else {
-    return { Authorization: "" };
+    return `Bearer ${access_token}`;
   }
 }
